31a768e20e0e838703bc30eb5bdc8c81baed12fa
Jason <guyi@kingsoft.com>
2018-01-24 17:53:17 +0800

    delete node_modules and add init.sh

---
+++ init.sh
@@ -0,0 +1,2 @@
+rm -rf node_modules
+npm install --save hexo-autoprefixer hexo-filter-cleanup hexo-generator-feed hexo-generator-sitemap hexo-renderer-sass hexo-renderer-swig mamboer/lunr.js moment node-sass object-assign
