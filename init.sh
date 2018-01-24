11b3c28120fd8ca541fdb28310a9b5a24eb9a3a2
Jason <guyi@kingsoft.com>
2018-01-24 17:55:28 +0800

    add modify init.sh

--- init.sh
+++ init.sh
@@ -1,2 +1,5 @@
+#!/bin/bash
+
+source deactivate
 rm -rf node_modules
 npm install --save hexo-autoprefixer hexo-filter-cleanup hexo-generator-feed hexo-generator-sitemap hexo-renderer-sass hexo-renderer-swig mamboer/lunr.js moment node-sass object-assign
